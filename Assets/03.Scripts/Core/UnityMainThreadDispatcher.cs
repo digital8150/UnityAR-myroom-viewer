e97bb38722de2052c566ff07a4758fb206bbe37b
digital8150 <wptpgud@gmail.com>
2026-01-15 18:02:32 +0900

    feature: 유니티 메인 스레드 디스패처

---
+++ Assets/03.Scripts/Core/UnityMainThreadDispatcher.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Concurrent;
+using UnityEngine;
+
+public class UnityMainThreadDispatcher : MonoBehaviour
+{
+    //--- Fields ---
+    private static readonly ConcurrentQueue<Action> _executionQueue = new ConcurrentQueue<Action>();
+
+    //--- Unity Methods ---
+    void Update()
+    {
+        DoActionsInQueue();
+    }
+
+    //--- Public Methods ---
+    /// <summary>
+    /// 메인 스레드에서 수행할 작업을 등록합니다.
+    /// </summary>
+    /// <param name="action">수행할 작업</param>
+    public static void Enqueue(Action action)
+    {
+        _executionQueue.Enqueue(action);
+    }
+
+    //--- Private Methods ---
+    private static void DoActionsInQueue()
+    {
+        while (_executionQueue.TryDequeue(out var action))
+        {
+            action.Invoke();
+        }
+    }
+}
