b05cdbf025476160b532c3c4c4c19c0c9fd8aa03
digital8150 <wptpgud@gmail.com>
2026-02-03 20:01:31 +0900

    feature: 로그인 > 홈 >  가구 3D 변환 흐름

---
+++ Assets/03.Scripts/UI/Home/HomePresenter.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HomePresenter
+{
+    private readonly IHomeView _view;
+
+    public HomePresenter(IHomeView view)
+    {
+        _view = view;
+    }
+
+    public void OnToGenerate3DClicked()
+    {
+        Debug.Log("Navigate to 3D Generation Scene");
+        SceneManager.LoadScene("Generate3D");
+    }
+}
