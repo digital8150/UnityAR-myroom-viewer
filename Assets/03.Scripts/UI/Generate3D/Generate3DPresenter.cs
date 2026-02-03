8eb0e3c657cd131100685371a503b996ba1fd186
digital8150 <wptpgud@gmail.com>
2026-02-03 20:30:48 +0900

    feature: 파일 선택기

---
+++ Assets/03.Scripts/UI/Generate3D/Generate3DPresenter.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+
+public class Generate3DPresenter
+{
+    private readonly IGenerate3DView _view;
+
+    public Generate3DPresenter(IGenerate3DView view)
+    {
+        _view = view;
+    }
+
+    public void OnReturnClicked()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Home");
+    }
+
+    public void OnLoadImageClicked()
+    {
+        if (NativeFilePicker.IsFilePickerBusy())
+        {
+            
+            return;
+        }
+
+        try
+        {
+            NativeFilePicker.PickFile((path) =>
+            {
+                if (path == null)
+                {
+                    // 사용자가 파일 선택을 취소함
+                }
+                else if (!IsValidFileExtensioin(path))
+                {
+                    // 지원하지 않는 파일 형식
+                }
+                else
+                {
+                    // 파일 선택 완료
+
+                }
+            });
+        }
+        catch (System.Exception e)
+        {
+            // 파일 선택기 오류 처리
+        }
+    }
+
+    public void OnTakePictureClicked()
+    {
+
+    }
+
+    //--- Private Methods ---//
+    private bool IsValidFileExtensioin(string path)
+    {
+        string lowerPath = path.ToLower();
+        return lowerPath.EndsWith(".png") || lowerPath.EndsWith(".jpg") || lowerPath.EndsWith(".jpeg");
+    }
+}
---
+++ Assets/03.Scripts/UI/Generate3D/Generate3DView.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public interface IGenerate3DView
+{
+}
+
+public class Generate3DView : MonoBehaviour, IGenerate3DView
+{
+    //--- Settings ---//
+    [Header("Buttons")]
+    [SerializeField]
+    private Button _returnBtn, _loadImageBtn, _takePictureBtn;
+
+    //--- Fields ---//
+    private Generate3DPresenter _presenter;
+
+    //--- Unity Methods ---//
+    private void Awake()
+    {
+        InitializeView();
+    }
+
+    private void OnDestroy()
+    {
+        DisposeListeners();
+    }
+
+    //--- Private Methods ---//
+    private void InitializeView()
+    {
+        _presenter = new Generate3DPresenter(this);
+        _returnBtn?.onClick.AddListener(_presenter.OnReturnClicked);
+        _loadImageBtn?.onClick.AddListener(_presenter.OnLoadImageClicked);
+        _takePictureBtn?.onClick.AddListener(_presenter.OnTakePictureClicked);
+    }
+
+    private void DisposeListeners()
+    {
+        _returnBtn?.onClick.RemoveAllListeners();
+        _loadImageBtn?.onClick.RemoveAllListeners();
+        _takePictureBtn?.onClick.RemoveAllListeners();
+    }
+}
